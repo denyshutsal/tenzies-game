20ac52b52df5a4e0bb4b6b348da6d187d5133d20
Denys Hutsal <denyshutsal@gmail.com>
2023-02-07 11:37:45 +0100

    feat: added the ability to select a die

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,8 +2,13 @@ import React from "react";
 import "./Die.scss";
 
 export default function Die(props) {
+  let className = "die";
+  if (props.isHeld) {
+    className += " die--selected";
+  }
+
   return (
-    <div className="die">
+    <div className={className}>
       <span className="die__text">{props.value}</span>
     </div>
   );
